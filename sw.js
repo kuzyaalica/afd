09a663da679fdca50e0be6d6fe95ac3434226e4c
kuzyaalica <kuzyaalica@gmail.com>
2026-09-05 10:36:34 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Меняй версию (например, v2, v3), когда вносишь правки в HTML
-const CACHE_NAME = 'courier-app-v4';
+const CACHE_NAME = 'courier-app-v5';
 const ASSETS = [
   './',
   './index.html',
